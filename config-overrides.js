64e3528e76667c4d458c0ec94f4da6d1cc534b98
KamilFil <kamilfprog@gmail.com>
2023-11-19 19:54:04 +0100

    add config-override

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,6 @@
 const {override} = require("customize-cra");
 const {aliasDangerous, configPaths} = require("react-app-rewire-alias/src/aliasDangerous");
 
-
-
 module.exports = {
     webpack: override(
         aliasDangerous(configPaths('./tsconfig.paths.json'))
